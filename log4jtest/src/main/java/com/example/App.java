6d7375d8b6c577f0d736aa01a72b1d4d20d1539b
hiroaki-yamada <manigeo273511@gmail.com>
2022-01-07 15:21:40 +0900

    Add: 基本的な log4j2 のコードを記述 #2

--- log4jtest/src/main/java/com/example/App.java
+++ log4jtest/src/main/java/com/example/App.java
@@ -1,13 +1,28 @@
 package com.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // System.out.println( "Hello World!" );
+        Logger logger = LogManager.getLogger();
+
+        logger.trace("start");
+
+        int a = 1;
+        int b = 2;
+        String c = null;
+
+        logger.debug("debug");
+        logger.info("info a = {}", a);
+        logger.warn("warn b = {}", b);
+        logger.error("error c = {}", c);
+        logger.fatal("fatal");
+        logger.trace("end");
     }
 }
